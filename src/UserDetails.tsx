8cce31e4e2503a0e4bdc63d672022a425e1c5f83
viru <38450997+HRithIkGowDA@users.noreply.github.com>
2023-05-02 20:48:20 +0530

    Create UserDetails.tsx

---
+++ src/UserDetails.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./styles.css";
+
+type User = {
+  name: {
+    title: string;
+    first: string;
+    last: string;
+  };
+  email: string;
+};
+
+const UserDetails: React.FC = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await axios.get("https://randomuser.me/api/");
+      const userData = response.data.results[0];
+      const { name, email } = userData;
+      setUser({ name, email });
+      localStorage.setItem("user", JSON.stringify({ name, email }));
+    };
+
+    fetchUser();
+  }, []);
+
+  return (
+    <>
+      {user && (
+        <>
+          <h1>
+            {user.name.title} {user.name.first} {user.name.last}
+          </h1>
+          <p>{user.email}</p>
+        </>
+      )}
+    </>
+  );
+};
+
+export default UserDetails;
